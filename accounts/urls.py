3a5133b1ec3dc9d5d1085bf1cecab188d7b4964f
SHIMA Ryosuke <shima@unalus.com>
2022-07-08 17:43:37 +0900

    登録画面を追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-'''LoginView, LogoutViewクラスベースビューを使用する'''
+from django.views.generic import CreateView
+
+
 urlpatterns = [
+    # LoginView, LogoutViewクラスベースビューを使用する
     path('login/', LoginView.as_view(
         redirect_authenticated_user=True,
         template_name='accounts/login.html'
         ), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    # 汎用クラスベースビューを使用
+    path('signup/', CreateView.as_view(
+        template_name='accounts/signup.html',
+        form_class=UserCreationForm,
+        success_url='/',
+        ), name='signup'),
 ]
