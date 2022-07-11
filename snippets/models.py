0e5ff6515e9a926246cda2044bf8ee87007b3811
SHIMA Ryosuke <shima@unalus.com>
2022-07-11 13:04:07 +0900

    コメントとタグテーブルを追加

--- snippets/models.py
+++ snippets/models.py
@@ -12,9 +12,43 @@ class Snippet(models.Model):
     updated_at = models.DateTimeField("更新日", auto_now=True)
 
     def __str__(self):
-        return self.title
+        return f'{self.pk} {self.title}'
 
     # Meta オプション
     class Meta:
         # テーブル名を明示的に指定する
         db_table = 'snippets'
+
+class Comment(models.Model):
+    text = models.TextField('本文', blank=False)
+    comment_to = models.ForeignKey(
+        Snippet,
+        verbose_name = 'スニペット',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'{self.pk} {self.text}'
+
+    class Meta:
+        db_table = 'comments'
+
+class Tag(models.Model):
+    name = models.CharField(
+        'タグ名',
+        max_length = 32
+    )
+
+    # snippetsテーブルとtagsテーブルを多対多の関係で紐付ける。
+    # 中間テーブルは自動で生成される
+    snippets = models.ManyToManyField(
+        Snippet,
+        related_name='tags',
+        related_query_name='tag'
+    )
+
+    def __str__(self):
+        return f'{self.pk} {self.name}'
+
+    class Meta:
+        db_table = 'tags'
