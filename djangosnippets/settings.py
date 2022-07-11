d82973bb3f26192779939a4f6cbcb2d5679551fc
SHIMA Ryosuke <shima@unalus.com>
2022-07-11 13:11:12 +0900

    DB操作のロギング設定を追加

--- djangosnippets/settings.py
+++ djangosnippets/settings.py
@@ -132,3 +132,20 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    }
+}
